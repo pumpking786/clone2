efb13c32162a9cfd055455c132fe852e406586a6
Pramit Amatya <pramitamatya786@gmail.com>
2025-11-13 11:09:41 +0545

    done

--- vite.config.js
+++ vite.config.js
@@ -2,29 +2,22 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
-  const repoName = env.VITE_REPO_NAME || "";
-
   return {
-    // 👉 Use subfolder path only in local or GitHub Pages,
-    // but root "/" for production (Vercel)
-    // base: mode === "production" ? "/" : `/${repoName}/`,
-    base: "/picto/",
-
+    base: `/${env.VITE_REPO_NAME}/`,
     plugins: [react(), tailwindcss()],
-
     server: {
       open: true,
-      host: true,
-      // proxy (uncomment if you need it later)
-      // proxy: {
-      //   "/api": {
-      //     target: "http://localhost:5000",
-      //     changeOrigin: true,
-      //     secure: true,
-      //   },
-      // },
+      host: false, // Allow access from network devices
+      proxy: {
+        "/api": {
+          target: "http://localhost:5000",
+          changeOrigin: true,
+          secure: true,
+        },
+      },
     },
   };
 });
